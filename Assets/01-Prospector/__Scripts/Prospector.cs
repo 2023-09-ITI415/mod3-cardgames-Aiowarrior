877787dc785723bd7d30ac97122f3ef5844fed07
Aiowarrior <uma8@scarletmail.rutgers.edu>
2023-10-27 21:37:41 -0400

    matching cards from mine script added

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -97,11 +97,44 @@ CardProspector Draw()
 			cp.SetSortingLayerName(tSD.layerName); // Set the sorting layers
             tableau.Add(cp); // Add this CardProspector to the List<> tableau
 		}
+			// Set which cards are hiding others
+			foreach (CardProspector tCP in tableau) {
+			foreach( int hid in tCP.slotDef.hiddenBy ) {
+			cp = FindCardByLayoutID(hid);
+			tCP.hiddenBy.Add(cp);
+}
+}
+
 			// Set up the initial target card
 			MoveToTarget(Draw ());
 			// Set up the Draw pile
 			UpdateDrawPile();
 	}
+	// Convert from the layoutID int to the CardProspector with that ID
+CardProspector FindCardByLayoutID(int layoutID) {
+foreach (CardProspector tCP in tableau) {
+// Search through all cards in the tableau List<>
+if (tCP.layoutID == layoutID) {
+// If the card has the same ID, return it
+return( tCP );
+}
+}
+// If it's not found, return null
+return( null );
+}
+// This turns cards in the Mine face-up or face-down
+void SetTableauFaces() {
+foreach( CardProspector cd in tableau ) {
+bool faceUp = true; // Assume the card will be face-up
+foreach( CardProspector cover in cd.hiddenBy ) {
+// If either of the covering cards are in the tableau
+if (cover.state == eCardState.tableau) {
+faceUp = false; // then this card is face-down
+}
+}
+cd.faceUp = faceUp; // Set the value on the card
+}
+}
 	// Moves the current target to the discardPile
 void MoveToDiscard(CardProspector cd) {
 // Set the state of the card to discard
@@ -183,6 +216,7 @@ if (!validMatch) return; // return if not valid
 // If we got here, then: Yay! It's a valid card.
 tableau.Remove(cd); // Remove it from the tableau List
 MoveToTarget(cd); // Make it the target card
+SetTableauFaces(); // Update tableau card face-ups
 break;
 }
 }
